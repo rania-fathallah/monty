ee13d693969bd6a71aa80d177104bf18205aa61b
mohamed gharieb <m.gharieb89@gmail.com>
2023-11-20 14:31:11 -0800

    sw

--- swap.c
+++ swap.c
@@ -1,9 +1,10 @@
 #include "monty.h"
 /**
- * f_swap - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ *f_swap - adds the top two elements of the stack.
+ *Description: function for add at the top of the stk
+ *@head: stack head
+ *@counter: line_number
+ *Return: no return
 */
 void f_swap(stack_t **head, unsigned int counter)
 {
